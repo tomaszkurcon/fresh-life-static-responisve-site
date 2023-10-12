6a2ae7b4e3625c12292b8cb18ab2080c79f51344
Tomasz <tomekkurcon1@gmail.com>
2023-10-12 19:41:08 +0200

    Added logo, observers,  text content

---
+++ js/helpers.js
@@ -0,0 +1,20 @@
+const id_section_to_number = (section) => {
+    switch (section) {
+        case "fresh-life":
+            return 0
+        case "about-us":
+            return 1
+        case "our-services":
+            return 2
+        case "gallery":
+            return 3
+        case "newsletter":
+            return 4
+        case "blog":
+            return 5
+        case "contact":
+            return 6
+        default:
+            return 0;
+      }
+}
---
+++ js/main.js
@@ -0,0 +1,42 @@
+
+
+const nav_bar = document.getElementById("navigation");
+const slide_in_elements = document.querySelectorAll(
+  ".hidden-left,.hidden-right,.hidden-down,.hidden-hero"
+);
+const menu_elements = document.querySelectorAll(".menu-element");
+const sections = document.querySelectorAll(".section");
+const infinite_scroll_observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry, index) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  },
+  { threshold: 0.5 }
+);
+const sections_observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        console.log(entry.target.id);
+        console.log(menu_elements)
+        menu_elements.forEach((el) => el.classList.remove("active"));
+        menu_elements[id_section_to_number(entry.target.id)].classList.add(
+          "active"
+        );
+      }
+    });
+  },
+  { threshold: 0.5 }
+);
+
+slide_in_elements.forEach((el) => infinite_scroll_observer.observe(el));
+sections.forEach((el) => sections_observer.observe(el));
+document.addEventListener("scroll", (event) => {
+  nav_bar.classList.add("transparency");
+});
+document.addEventListener("scrollend", (event) => {
+  nav_bar.classList.remove("transparency");
+});
