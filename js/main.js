403fefcdf27f59c2d8a3da8c989d124047d689f5
Tomasz <tomekkurcon1@gmail.com>
2023-10-17 12:06:32 +0200

    Added mobile navigation

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
-
-
 const nav_bar = document.getElementById("navigation");
+const hamburger = document.getElementById("hamburger");
+const sidebar = document.querySelector(".sidebar");
 const slide_in_elements = document.querySelectorAll(
   ".hidden-left,.hidden-right,.hidden-down,.hidden-hero"
 );
 const menu_elements = document.querySelectorAll(".menu-element");
 const sections = document.querySelectorAll(".section");
+
 const infinite_scroll_observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry, index) => {
@@ -21,7 +22,7 @@ const sections_observer = new IntersectionObserver(
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
         console.log(entry.target.id);
-        console.log(menu_elements)
+        console.log(menu_elements);
         menu_elements.forEach((el) => el.classList.remove("active"));
         menu_elements[id_section_to_number(entry.target.id)].classList.add(
           "active"
@@ -39,4 +40,9 @@ document.addEventListener("scroll", (event) => {
 });
 document.addEventListener("scrollend", (event) => {
   nav_bar.classList.remove("transparency");
+});
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  sidebar.classList.toggle("sidebar-active");
 });
