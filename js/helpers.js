0c3ca2ec0ed09b408492efefb21cbc21ea1a47a3
Tomasz <tomekkurcon1@gmail.com>
2023-10-21 20:57:58 +0200

    Added notifications and gallery slider

--- js/helpers.js
+++ js/helpers.js
@@ -18,3 +18,44 @@ const id_section_to_number = (section) => {
       return 0;
   }
 };
+
+const createNotification = (
+  text,
+  classes = ["fa-solid", "fa-check", "fa-lg"]
+) => {
+  const notifications_list = document.querySelectorAll(".notification");
+  //Check if there is too much notifcations on screen
+  if (notifications_list.length > 6) {
+    return;
+  }
+
+  //create notification
+  const notification = document.createElement("div");
+  const paragraph = document.createElement("p");
+  paragraph.textContent = text;
+  const icon = document.createElement("i");
+  icon.classList.add(...classes, "notification-icon");
+  notification.appendChild(paragraph);
+  notification.appendChild(icon);
+  notification.classList.add("notification");
+  notifications.appendChild(notification);
+
+  //animate notifications up
+  notifications_list.forEach((notification, index) => {
+    if (index != notifications_list.length) {
+      notification.animate(
+        [
+          {
+            transform: `translateY(${
+              -(notifications_list.length - index) * 120
+            }px)`,
+          },
+        ],
+        { duration: 300, iterations: 1, fill: "forwards" }
+      );
+    }
+  });
+  setTimeout(() => {
+    notification.remove();
+  }, 5000);
+};
--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 const nav_bar = document.getElementById("navigation");
 const hamburger = document.getElementById("hamburger");
+const notifications = document.getElementById("notifications");
 const sidebar = document.querySelector(".sidebar");
-const overlay = document.querySelector(".overlay")
+const overlay = document.querySelector(".overlay");
 const slide_in_elements = document.querySelectorAll(
   ".hidden-left,.hidden-right,.hidden-down,.hidden-hero"
 );
 const menu_elements = document.querySelectorAll(".menu-element");
 const sections = document.querySelectorAll(".section");
 
+const newsletter_form = document.forms.newsletter;
+const contact_form = document.forms.contact_form;
+//IntersectionObserver for sliding in elements while scrolling
 const infinite_scroll_observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry, index) => {
@@ -18,12 +22,11 @@ const infinite_scroll_observer = new IntersectionObserver(
   },
   { threshold: 0.5 }
 );
+//IntersectionObserver to switch active section in menu
 const sections_observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        console.log(entry.target.id);
-        console.log(menu_elements);
         menu_elements.forEach((el) => el.classList.remove("active"));
         menu_elements[id_section_to_number(entry.target.id)].classList.add(
           "active"
@@ -36,6 +39,8 @@ const sections_observer = new IntersectionObserver(
 
 slide_in_elements.forEach((el) => infinite_scroll_observer.observe(el));
 sections.forEach((el) => sections_observer.observe(el));
+
+//making navigation more transparent while scrolling
 document.addEventListener("scroll", (event) => {
   nav_bar.classList.add("transparency");
 });
@@ -47,4 +52,31 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   sidebar.classList.toggle("sidebar-active");
   overlay.classList.toggle("overlay-active");
+});
+
+//creating notification on newsletter form submit
+newsletter_form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const email = newsletter_form.elements.email.value;
+  createNotification(
+    `Your email: ${email} has been succesfully addded to our newsletter!`
+  );
+  newsletter_form.elements.email.value = "";
+});
+
+//extracting data from contact form, saving it to local storage and creating notification 
+contact_form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(contact_form.elements);
+  const email = contact_form.elements.email.value;
+  const phoneNumber = contact_form.elements.phone_number.value;
+  const message = contact_form.elements.message.value;
+  const user = {
+    email: email,
+    phoneNumber: phoneNumber,
+    message: message,
+  };
+  localStorage.setItem("user_data", JSON.stringify(user));
+  createNotification(`Thank you for writing to us! We will respond ASAP!`);
+
 });
